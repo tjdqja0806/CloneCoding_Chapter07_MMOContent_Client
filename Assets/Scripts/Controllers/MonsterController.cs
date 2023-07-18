52a07a8286587e1a5fd00a8aabeb6e0a801beb8c
tjdqja0806 <74092254+tjdqja0806@users.noreply.github.com>
2023-07-18 10:59:09 +0900

    First Upload
    Arrow Effect

---
+++ Assets/Scripts/Controllers/MonsterController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class MonsterController : CreatureController
+{
+    protected override void Init()
+    {
+        base.Init();
+
+        State = CreatureState.Idle;
+        Dir = MoveDir.None;
+    }
+
+    protected override void UpdateContoller()
+    {
+        //GetDirInput();
+        base.UpdateContoller();
+    }
+
+    //키보드 입력을 받아 방향 설정
+    void GetDirInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Dir = MoveDir.Up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Dir = MoveDir.Down;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Dir = MoveDir.Left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Dir = MoveDir.Right;
+        }
+        else
+        {
+            Dir = MoveDir.None;
+        }
+    }
+
+    public override void OnDamaged()
+    {
+        GameObject effect = Managers.Resource.Instantiate("Effect/DieEffect");
+        effect.transform.position = transform.position;
+        effect.GetComponent<Animator>().Play("START");
+        GameObject.Destroy(effect, 0.5f);
+
+        Managers.Object.Remove(gameObject);
+        Managers.Resource.Destroy(gameObject);
+    }
+}
